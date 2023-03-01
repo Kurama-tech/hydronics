0dd7688de8d498e9a0d1e6f134ad943a4e1d8774
Meer Sawood <msawood@redhat.com>
2023-03-01 17:01:50 +0400

    something

--- src/components/const.js
+++ src/components/const.js
@@ -1,6 +1,5 @@
 
-//const Destination = "info@hydronicsystems.ae"
-const Destination = "sameen68669@gmail.com"
+const Destination = "info@hydronicsystems.ae"
 const URLPrefix = "https://us-central1-mamun-public.cloudfunctions.net/sendMail"
 
 export {Destination, URLPrefix}
