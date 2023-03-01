f55954e31331b54a5a9420ef108c9cf868862f18
Meer Sawood <msawood@redhat.com>
2023-03-01 17:37:52 +0400

    link color change

--- src/sections/subscribe.js
+++ src/sections/subscribe.js
@@ -31,14 +31,14 @@ const Subscribe = () => {
         <Flex sx={{ alignItems: 'center', my: 2 }}>
           <FaPhone sx={{ mr: 2 }} color="white" />
           <Box>
-          <Text variant="heading" color="white" href="tel:+971529620433" >+971 529620433</Text>
+          <Text variant="heading" color="white"><a href="tel:+971529620433" sx={{color: '#FFFF'}}>+971 529620433</a></Text>
           <br />
           </Box>
         </Flex>
         <Flex sx={{ alignItems: 'center', my: 2 }}>
           <FaEnvelope sx={{ mr: 2 }} color="white" />
           <Box>
-            <Text variant="heading" color="white" href="mailto:info@hydronicsystems.ae">info@hydronicsystems.ae</Text>
+            <Text variant="heading" color="white"><a href="mailto:info@hydronicsystems.ae" sx={{color: '#FFFF'}}>info@hydronicsystems.ae</a></Text>
           </Box>
         </Flex>
       </Box>
