055b387c373a0ad882dc34fe6654ac35bd318444
Meer Sawood <msawood@redhat.com>
2023-03-01 17:45:31 +0400

    link color change

--- src/components/const.js
+++ src/components/const.js
@@ -1,6 +1,5 @@
 
-//const Destination = "info@hydronicsystems.ae"
-const Destination = "ameenfarooqi25@gmail.com"
+const Destination = "info@hydronicsystems.ae"
 const URLPrefix = "https://us-central1-mamun-public.cloudfunctions.net/sendMail"
 
 export {Destination, URLPrefix}
